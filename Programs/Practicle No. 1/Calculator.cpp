95b082731d4c769ace4ba1e5171bd8d9699b8fd4
activ8 <pranav.vasankar@gmail.com>
2024-09-04 23:15:36 +0530

    Update Calculator.cpp

--- Programs/Practicle No. 1/Calculator.cpp
+++ Programs/Practicle No. 1/Calculator.cpp
@@ -1,5 +1,5 @@
-// Write a C++ program create a calculator for an arithmetic operator (+, -, *, /). The program should take two operands from user and performs the operation 
-// on those two operands depending upon the operator entered by user. Use a switch statement to select the operation. Finally, display the result .
+// Q.    Write a C++ program create a calculator for an arithmetic operator (+, -, *, /). The program should take two operands from user and performs the operation on those two 
+// operands depending upon the operator entered by user. Use a switch statement to select the operation. Finally, display the result .
 
 //file name : "Calculator.cpp"
 

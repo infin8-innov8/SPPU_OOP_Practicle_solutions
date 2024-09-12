c4cb4927d60a3c262e13043f75a48912cfb1d242
activ8 <pranav.vasankar@gmail.com>
2024-09-12 23:18:22 +0530

    Update Calculator.cpp

--- Programs/Practicle No. 3/Calculator.cpp
+++ Programs/Practicle No. 3/Calculator.cpp
@@ -1,73 +1,76 @@
-// Write a C++ program create a calculator for an arithmetic operator (+, -, *, /). The program should take two operands from user and performs the operation 
-// on those two operands depending upon the operator entered by user. Use a switch statement to select the operation. Finally, display the result .
-
-//file name : "Calculator.cpp"
+// Q.   Write a C++ program create a calculator for an arithmetic operator (+, -, *, /). The program should take two operands from user and performs the operation 
+//      on those two operands depending upon the operator entered by user. Use a switch statement to select the operation. Finally, display the result .
 
 #include <iostream> 
 
 namespace Calculators { 
     class Calculator { 
-        private : 
-            double num1;
-            double num2; 
-            char opr; // ie. operator
-    
-        public : 
-            // creating default constructor
-            Calculator (void) { 
-                std::cout << "Default constructor called." << std::endl;
-                num1 = 0; 
-                num2 = 0;
-                opr = '\0';
-            }
-            // far scanning for num1, num2 and opr
-            void scan() { 
-                std::cout << "Enter the fist number : "; 
-                std::cin >> num1;
-                
-                std::cout << "Enter the operation to be performed (+ , - , * , /): ";
-                std::cin >> opr;
-                
-                std::cout << "Enter the second number : "; 
-                std::cin >> num2; 
-  
-            }
-            // for canluation and printing the result 
-            void calculate() { 
-                switch (opr) { 
-                    case '+' :
-                        std::cout << "Tthe sum of the given two number is : " << num1 + num2 << std::endl; 
-                        break;
-                    
-                    case '-' : 
-                        std::cout << "The subtraction of the given two numbers is : " << num1 - num2 << std::endl; 
-                        break; 
+    private : 
+        double num1; // data membeer of first operend
+        double num2; // data member of second operend
+        char opr; // data member for operator
 
-                    case '*' : 
-                        std::cout << "The multiplication of the given two numbers is : " << num1 * num2 << std::endl;
-                        break; 
-                    
-                    case '/' : 
-                        if(num2 == 0) { 
-                            std::cout << "Any number divided by 0 is undifined." << std::endl;
-                        } 
-                        else { 
-                            std::cout << "The division of the given numbers is : " << num1 / num2 << std::endl; 
-                        }
-                        break;
+    public : 
+        // creating explici default constructor
+        Calculator (void) { 
+            std::cout << "(The explicit default constructor invoked.)" << std::endl;
+            num1 = 0; 
+            num2 = 0;
+            opr = '\0';
+        }
+        
+        // for scanning for num1, num2 and opr
+        void scan() { 
+            std::cout << "(scanner function invoked.)" << std::endl; 
+            
+            std::cout << "\tEnter the fist number : "; 
+            std::cin >> num1;
+            
+            std::cout << "\tEnter the operation to be performed (+ , - , * , /): ";
+            std::cin >> opr;
+            
+            std::cout << "\tEnter the second number : "; 
+            std::cin >> num2; 
 
-                    default : 
-                        std::cout << "We can conclude that the given inputs aren't valid, try again :)" << std::endl; 
-                }
+        }
+        // for canluation and printing the result 
+        void calculate() { 
+            switch (opr) { 
+                case '+' :
+                    std::cout << "\tThe sum of the given two number is : " << num1 + num2 << std::endl; 
+                    break;
+                
+                case '-' : 
+                    std::cout << "\tThe subtraction of the given two numbers is : " << num1 - num2 << std::endl; 
+                    break; 
+
+                case '*' : 
+                    std::cout << "\tThe multiplication of the given two numbers is : " << num1 * num2 << std::endl;
+                    break; 
+                
+                case '/' : 
+                    if(num2 == 0) { 
+                        std::cout << "\tAny number divided by 0 is undifined." << std::endl;
+                    } 
+                    else { 
+                        std::cout << "\tThe division of the given numbers is : " << num1 / num2 << std::endl; 
+                    }
+                    break;
+
+                default : 
+                    std::cout << "\tWe can conclude that the given inputs aren't valid, try again :)" << std::endl; 
             }
+        }
     };
 }
 int main() { 
-    Calculators::Calculator operation1; // an object has been created
+    Calculators::Calculator operations[6]; 
+    
+    for (Calculators::Calculator object : operations) { 
+        object.scan(); 
+        object.calculate() ; 
+    }
 
-    operation1.scan(); 
-
-    operation1.calculate();
-
+    std::cout << "===| Code Execution Successfull |===" << std::endl << std::endl; 
     return 0;
 }

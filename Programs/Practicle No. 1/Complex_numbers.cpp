3df9870294c3d1f64573d8538898252597fc2e80
activ8 <pranav.vasankar@gmail.com>
2024-09-12 22:39:02 +0530

    Update Complex_numbers.cpp

--- Programs/Practicle No. 1/Complex_numbers.cpp
+++ Programs/Practicle No. 1/Complex_numbers.cpp
@@ -1,54 +1,65 @@
-// Title : Implement a class complex which represent the Complex Number data type. Implement the following operations:
-//         1. Constructor (include a default constructor which creates the complex number 0+0i )
-//         2. Overloaded operator + to add two polynomials of degree 2.
-//         3. Overloaded operator * to multiply two complex number.
-//         4. Overloaded << and >> to print and read Complex Number.
+// Title: Implement a class CppArray which is identical to one dimensional C++ array ( i.e ,the index set is a set of consecutive integers starting at 0)except for the following :
+//       It performs range checking .
+//       It allows one to be assigned to another array through the use of the assignment operator (i.e cp1=cp2).
+//       It supports a function that returns the size of array.
+//       It allows the reading and printing of array through the use cout and cin.
 
-// file name : "Complex_numbers.cpp"
 #include <iostream>
 
 namespace complex_numbers {
     class Complex {
         public : 
-            // complex numbers consist of two parts, real and imaginary part.
+            // complex numbers consist of real and imaginary part. 
             double real; 
             double imag; 
 
-            // creating constructor of the class complex. 
+            // creating explicit constructor of the class complex. 
             Complex(void) { 
+                std::cout << "(Explicit default constructor invoked.)" << std::endl;
                 real = 0;
                 imag = 0;
             }
 
-            // operator overloading of unary plus for addition of the complex numbers. 
-            Complex operator + ( Complex num) {
+            // creating explicit copy constructor of the class
+            Complex( const Complex& other) { 
+                std::cout << "(Explicit copy constructor invoked.)" << std::endl;
+                real = other.real; 
+                imag = other.imag;
+
+            }
+
+            // operator overloading for addition of the complex numbers. 
+            Complex operator + ( const Complex &num) {
+                std::cout << "(The operator + is overloaded.)" << std::endl; 
                 Complex temp;
                 temp.real = real + num.real;
                 temp.imag = imag + num.imag;
                 return temp;
             }
             
-            // operator overloading of '*' for multiplication of complex numbers. 
-            Complex operator * ( Complex num) {
+            // operator overloading for multiplication of complex numbers. 
+            Complex operator * (const Complex &num) {
+                std::cout << "(The operator * is overloaded.)" << std::endl; 
                 Complex temp;
                 temp.real = real * num.real;
                 temp.imag = imag * num.imag;
                 return temp;
             }
 
-            // operator overloading of insertion operator for printing the resultant complex number.
-            friend std::ostream &operator  << (std::ostream &output, Complex &num) {
+            // operator overloading for printing the resultant complex number.
+            friend std::ostream &operator  << (std::ostream &output, const Complex& num) {
                 output << "The number is : " << num.real << " + " << num.imag << "i " << std::endl;
                 return output;
             }
 
-            // operator overloading of extraction operator for scanning complex numbers from user.
-            friend std::istream &operator >> (std::istream &input, Complex &num) {
-                std::cout << "\tEnter the real part of the number : ";
+            // operator overloading for scanning complex numbers from user.
+            friend std::istream& operator >> (std::istream& input, Complex& num) {
+                std::cout << "\t\tEnter the real part of the number : ";
                 input >> num.real;
 
-                std::cout << "\tEnter the imaginary part of the number : ";
+                std::cout << "\t\tEnter the imaginary part of the number : ";
                 input >> num.imag;
+
                 return input;
             }
     };
@@ -57,19 +68,20 @@ namespace complex_numbers {
 int main() { 
     complex_numbers::Complex num1; // object is created as num1.
 
-    std::cout << "Enter the first number : " << std::endl;
+    std::cout << "\tEnter the first number : " << std::endl;
     std::cin >> num1;
 
     complex_numbers::Complex num2; // another object is created as num2.
-    std::cout << "Enter the second number : " << std::endl;
+    std::cout << "\tEnter the second number : " << std::endl;
     std::cin >> num2; 
 
-    complex_numbers::Complex resultant_num; // for storing the result.  
+    complex_numbers::Complex resultant_num; // complex resultant_num is for storing the result.  
 
     resultant_num = (num1 + num2);
-    std::cout << "The sum of the given two complex numbers is : " << resultant_num << std::endl; 
+    std::cout << "\tThe sum of the given two complex numbers is : " << resultant_num << std::endl; 
 
     resultant_num = (num1 * num2);
-    std::cout << "The multiplication of the given two complex numbers is : " << resultant_num << std::endl; 
-    
+    std::cout << "\tThe multiplication of the given two complex numbers is : " << resultant_num << std::endl; 
+
+    std::cout << "===| Code Executed Successfuly |===" << std::endl;
 }
